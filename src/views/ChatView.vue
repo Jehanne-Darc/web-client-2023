5223bd2b54d6a233facde4b12dcfc92c4ac5a108
DONG ZHENGBO <chunghwa1997@gmail.com>
2023-12-18 10:43:04 +0900

    チャット画面をコミットしました

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,34 +1,33 @@
 <template>
   <div id="app">
-    <nav>
-      <div class="navbar">ChatGPT Chat App</div>
-    </nav>
-
-    <div class="chat-container">
-      <div class="message-list">
-        <div
-          v-for="(message, index) in messages"
-          :key="index"
-          class="message"
-          :class="{
-            'user-message': message.type === 'user',
-            'bot-message': message.type === 'bot',
-          }"
-        >
-          {{ message.text }}
-        </div>
+    <div class="title">SNSソフト・社内ボット</div>
+    <div class="content">
+      <div
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="{ 'user-message': message.isUser }"
+      >
+        {{ message.text }}
       </div>
-      <div class="input-box">
-        <div class="file-upload">
-          <label for="file-input"> + </label>
-          <input id="file-input" type="file" @change="handleFileUpload" />
-        </div>
+    </div>
+    <div class="footer">
+      <label for="fileInput" class="file-upload-button">
+        +
         <input
-          v-model="userMessage"
-          @keyup.enter="sendMessage"
-          placeholder="Type your message..."
+          type="file"
+          id="fileInput"
+          @change="handleFileChange"
+          style="display: none"
         />
-        <button @click="sendMessage" class="send-button">送信</button>
+      </label>
+      <input
+        type="text"
+        v-model="userMessage"
+        @keyup.enter="sendMessage"
+        placeholder="メッセージを入力"
+      />
+      <div class="btu" @click="sendMessage">
+        <img src="../assets//plane.png" alt="" />
       </div>
     </div>
   </div>
@@ -38,124 +37,146 @@
 export default {
   data() {
     return {
-      userMessage: "",
-      messages: [],
+      userMessage: "", // 用户输入的消息
+      messages: [], // 存储用户发送的消息的数组
+      selectedFile: null,
     };
   },
   methods: {
-    sendMessage() {
-      if (this.userMessage.trim() !== "") {
-        this.messages.push({ text: this.userMessage, type: "user" });
-        // Simulate a response from the bot
-        this.messages.push({ text: "Bot response...", type: "bot" });
-        this.userMessage = "";
+    handleFileChange(event) {
+      this.selectedFile = event.target.files[0];
+    },
+    uploadFile() {
+      if (this.selectedFile) {
+        console.log("選択済ファイル:", this.selectedFile.name);
+      } else {
+        console.log("ファイルをアップロードしてください。");
       }
     },
-    handleFileUpload(event) {
-      const file = event.target.files[0];
-      // Handle the uploaded file as needed
-      console.log("Uploaded file:", file);
+    sendMessage() {
+      // 在这里可以执行发送消息的相关操作，例如将消息添加到数组
+      if (this.userMessage.trim() !== "") {
+        this.messages.push({
+          text: this.userMessage,
+          isUser: true, // 标记为用户发送的消息
+        });
+        this.userMessage = ""; // 清空输入框
+      }
     },
   },
 };
 </script>
 
 <style>
-body {
-  background-color: #ffffff; /* 设置为白色 */
+body,
+html {
+  background-color: #ffffff; /* 白色背景 */
+  /* 设置为白色 */
   font-family: "Arial", sans-serif;
+  height: 100%;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-nav {
-  background-color: #3498db;
-  color: white;
-  padding: 10px;
+#app {
+  max-width: 1200px;
+  min-width: 900px;
+  height: auto;
+  margin: 0 auto;
+}
+
+.title {
+  z-index: 20;
+  width: 1200px;
+  height: 3rem;
+  line-height: 3rem;
   text-align: center;
+  background-color: #8f138f;
+  color: #ffffff;
+  font-size: 1.5rem;
+  border-radius: 50px;
+  position: fixed;
+  top: 0;
 }
 
-.chat-container {
+.content {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-  height: 100vh;
-}
-
-.message-list {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 80%;
-  margin-bottom: 20px;
-}
-
-.message {
-  background-color: #f0f0f0;
-  padding: 10px;
-  margin-bottom: 5px;
-  border-radius: 5px;
+  z-index: 1;
 }
 
 .user-message {
   align-self: flex-end;
-  background-color: #3498db;
-  color: white;
+  background-color: #dddddd; /* 灰色背景 */
+  color: #000000; /* 黑色字体 */
+  padding: 0.5rem;
+  margin: 0.5rem 0;
+  border-radius: 10px;
+  max-width: 70%;
 }
 
-.bot-message {
-  align-self: flex-start;
-  background-color: #e0e0e0;
+.footer {
+  width: 1200px;
+  height: 4rem;
+  color: #ffffff;
+  font-size: 2rem;
+  border-radius: 50px;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 }
 
-.input-box {
+.footer > input {
+  width: 87%;
+  height: 3rem;
+  border: 1px solid #8f138f;
+  outline: none;
+  border-radius: 50px;
+  padding: 0 1rem;
+  font-size: 1.5rem;
+  background-color: #d9cad9;
+  color: #000000;
+}
+
+.footer > button {
+  border: none;
+  border-radius: 50px;
+  background-color: #8f138f;
+  width: 3rem;
+  height: 3rem;
+  font-size: 2rem;
+  color: #ffffff;
+  cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 80%;
-  margin-top: 20px;
 }
 
-.file-upload {
+.file-upload-button,
+.btu {
+  cursor: pointer;
+  border: none;
+  border-radius: 50px;
+  background-color: #8f138f;
+  width: 3rem;
+  height: 3rem;
+  font-size: 2rem;
+  color: #ffffff;
   display: flex;
   align-items: center;
-  margin-right: 10px;
-}
-
-.file-upload label {
-  cursor: pointer;
-  background-color: #3498db;
-  color: white;
-  padding: 10px;
-  border-radius: 5px;
-  border: none;
+  justify-content: center;
 }
 
 input[type="file"] {
   display: none;
 }
 
-input {
-  flex: 1;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-right: 10px;
-}
-
-button.send-button {
-  padding: 10px;
-  background-color: #3498db;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-img {
-  width: 20px;
-  height: 20px;
+.footer > .btu > img {
+  width: 1.4rem;
+  height: 1.4rem;
 }
 </style>
