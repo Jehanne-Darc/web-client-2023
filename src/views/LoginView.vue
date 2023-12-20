36f8e4b6ceb4ea24ec2f1f5f687dfe7124917dfc
Darech <516837095@qq.com>
2023-12-20 17:11:52 +0900

    登陆页面适配手机

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <div style="width: 500px; margin: 50px auto; padding: 30px 50px; background: #fff;">
+    <div class="login-form">
       <h1 style="text-align: center; margin: 20px 0;">ユーザー登録</h1>
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
         <el-form-item label="ユーザー" prop="username">
@@ -21,11 +21,11 @@
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
+      isDesktop: window.innerWidth > 600,
       ruleForm: {
         username: '',
         password: ''
@@ -42,30 +42,42 @@ export default {
       }
     };
   },
+  mounted() {
+    window.addEventListener('resize', this.updateIsDesktop);
+  },
   methods: {
+    updateIsDesktop() {
+      this.isDesktop = window.innerWidth > 600;
+    },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          let url = 'http://localhost:8888/users/login';
+          let url = 'http://localhost:9999/users/login';
           console.log('尝试登录')
           console.log('请求路径为:' + url)
           console.log('请求参数为:' + this.ruleForm)
           console.log(this.ruleForm)
-          let dateFrom = this.qs.stringify(this.ruleForm)
-          this.axios.post(url, dateFrom).then((response) => {
+          // let dateFrom = this.qs.stringify(this.ruleForm)
+          // 设置请求头为 application/json
+          let config = {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
+          this.axios.post(url, this.ruleForm, config).then((response) => {
             console.log(response);
             if (response.data.state == 20000) {
-              console.log('登陆成功');
+              console.log('ログインが成功しました.');
               this.$message({
-                message: '登录成功',
+                message: 'ログインが成功しました.',
                 type: 'success'
               });
               this.$router.push({name: 'ChatApp'})
             } else {
-              console.log('登录失败,用户名或密码错误')
+              console.log('ログインに失敗しました。ユーザー名またはパスワードが正しくありません.')
               this.$notify.error({
-                title: '登录失败',
-                message: '登录失败,用户名或密码错误'
+                title: 'ログインに失敗しました.',
+                message: 'ログインに失敗しました。ユーザー名またはパスワードが正しくありません.'
               });
             }
           });
@@ -79,7 +91,7 @@ export default {
       this.$refs[formName].resetFields();
     }
   }
-}
+};
 </script>
 
 <style>
@@ -87,7 +99,23 @@ body {
   background-color: #0f66bb !important;
 }
 
-.login {
-  background-image: url("../../public/login_bg.png");
+.login-form {
+  width: 500px;
+  margin: 50px auto;
+  padding: 30px 50px;
+  background: #fff;
+}
+
+@media only screen and (max-width: 600px) {
+  .login-form {
+    width: 100%;
+    margin: 20px auto;
+    padding: 20px;
+  }
+
+  h1 {
+    text-align: center;
+    margin: 10px 0;
+  }
 }
 </style>
